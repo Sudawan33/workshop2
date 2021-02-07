5dde074c3c594542eedcb2da267104254c4a4da2
sudawan33 <PLOY@s33>
2021-02-07 22:22:06 +0700

    add Python Operators

---
+++ Python Operators/Membership operators1.py
@@ -0,0 +1,7 @@
+dictObject = {"name": "Ploy & Pim", "age": "21"}
+
+print("name" in dictObject)
+# Output: True
+
+print("Ploy & Pim" in dictObject)
+# Output: False
---
+++ Python Operators/Membership operators3.py
@@ -0,0 +1,7 @@
+name_list = ["Ploy", "Pim"]
+
+print("Ploy" in name_list)
+# Output: True
+
+print("Korn" in name_list)
+# Output: False
